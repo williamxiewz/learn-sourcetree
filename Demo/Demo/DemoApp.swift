5e9a2f472279894977d62d1d327de355b5558a4a
williamxiewz <williamxiewz@gmail.com>
2023-07-27 19:38:18 +0800

    初始化项目

---
+++ Demo/Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by williamxie on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
