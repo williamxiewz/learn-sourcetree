7222563eafa72a385b2d469af975823e1e99b547
williamxiewz <williamxiewz@gmail.com>
2023-07-30 18:20:08 +0800

    feat: 新功能

---
+++ Demo/Demo/SettingsView.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsView.swift
+//  Demo
+//
+//  Created by williamxie on 7/30/23.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
